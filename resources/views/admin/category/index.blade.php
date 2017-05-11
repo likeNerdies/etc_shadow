24fce2913bf5c8ea7827d2743090d2c4c65d62ab
ClaraAmeller <claraameller7@gmail.com>
2017-05-11 18:04:02 +0000

    Categories

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -69,8 +69,10 @@
   </div>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
 <script src="{{asset('/js/category/ajax-crud.js')}}"></script>
+
+
 <script src="{{asset('/fonts/Pe-icon-7-stroke.woff')}}"></script>
 
 <!--<script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>-->
