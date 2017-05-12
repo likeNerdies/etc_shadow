2fb815459666e7a5998854750d8942ee6c2bb953
Razon <x6698720q@gmail.com>
2017-05-12 10:14:59 +0000

    authorized

--- app/Http/Requests/Plan/PlanValidation.php
+++ app/Http/Requests/Plan/PlanValidation.php
@@ -13,7 +13,7 @@ class PlanValidation extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/Product/UploadProduct.php
+++ app/Http/Requests/Product/UploadProduct.php
@@ -13,7 +13,7 @@ class UploadProduct extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
