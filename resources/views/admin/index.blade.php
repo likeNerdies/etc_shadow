95fec09c22c8f2b0bbad70988d9007d5468e89ab
ClaraAmeller <claraameller7@gmail.com>
2017-05-13 10:32:53 +0000

    Everything downloaded, no CDN's v2

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -41,7 +41,7 @@
   </div>
 
   <script src="{{asset('/js/libraries/jquery/jquery-3.2.1.js')}}"></script>
-  <script src="{{asset('/js/tether.js')}}"></script><!-- IMPORTANT: Always before the bootstrap file -->
+  <script src="{{asset('/js/libraries/tether/tether.js')}}"></script><!-- IMPORTANT: Always before the bootstrap file -->
   <script src="{{asset('/js/libraries/bootstrap4/bootstrap.js')}}"></script>
 
   <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>-->
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,5 +1,5 @@
 <script src="{{asset('/js/libraries/jquery/jquery-3.2.1.js')}}"></script>
-<script src="{{asset('/js/tether.js')}}"></script><!-- IMPORTANT: Always before the bootstrap file -->
+<script src="{{asset('/js/libraries/tether/tether.js')}}"></script><!-- IMPORTANT: Always before the bootstrap file -->
 <script src="{{asset('/js/libraries/bootstrap4/bootstrap.min.js')}}"></script>
 <script src="{{asset('/js/libraries/jquery/jquery.easing.1.3.js')}}"></script>
 <script src="{{asset('/js/libraries/scroll-reveal/scrollreveal.min.js')}}"></script>
