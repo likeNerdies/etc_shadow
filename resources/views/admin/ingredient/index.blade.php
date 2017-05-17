847aec79955a8d0e5ece435164cf48eb29754ddb
adriavinias <adria.vinas.pascual@gmail.com>
2017-05-17 16:09:46 +0200

    Sidebar done

--- resources/views/admin/ingredient/index.blade.php
+++ resources/views/admin/ingredient/index.blade.php
@@ -53,13 +53,14 @@
                       @endif
                     </td>
 
-                    <td class="media-480-delete">{{$ingredient->created_at}}</td>
-
                       @if($ingredient->image_path==null)
                           <td id="ingredient-img"></td>
                       @else
-                          <td id="ingredient-img"><img src="{{$ingredient->getPublicImgUrl($ingredient->image_path)}}" width="165" height="110"></td>
+                          <td id="ingredient-img"><img class="img-thumbnail" src="{{$ingredient->getPublicImgUrl($ingredient->image_path)}}" width="48.2"></td>
                       @endif
+
+                      <td class="media-480-delete">{{$ingredient->created_at}}</td>
+
                       <td>
                           <button class="btn btn-warning btn-xs btn-detail open-modal" value="{{$ingredient->id}}">Edit</button>
                           <button class="btn btn-danger btn-xs btn-delete delete-ingredient" value="{{$ingredient->id}}">Delete</button>
