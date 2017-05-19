4f30fb505012f8bb8833dd8bcf96cb00b4b80c40
Razon <x6698720q@gmail.com>
2017-05-19 08:48:41 +0000

    seeder arreglador y añadido las rutas para las graficas

--- database/seeds/TableSeeders.php
+++ database/seeds/TableSeeders.php
@@ -7,6 +7,7 @@ use App\Admin;
 use App\Plan;
 use App\Address;
 use App\Brand;
+use App\Transporter;
 class TableSeeders extends Seeder
 {
     /**
@@ -18,15 +19,18 @@ class TableSeeders extends Seeder
     {
         $faker = Faker::create('en_US');
 
-        App\Admin::create(["name"=>"Razon","first_surname"=>"Miah","password"=>Hash::make("123456"),"email"=>"miahrazon@gmail.com","can_create"=>1]);
-        App\Admin::create(["name"=>"Clara","first_surname"=>"Ameller","password"=>Hash::make("123456"),"email"=>"cameller@gmail.com","can_create"=>1]);
-        App\Admin::create(['name'=>"Adria",'first_surname'=>"Vinas",'email'=>"avinas@gmail.com",'password'=>Hash::make("NtRnStR1617"),"can_create"=>1]);
-        App\Admin::create(['name'=>"Admin",'first_surname'=>"Test",'email'=>"testadmin@gmail.com",'password'=>Hash::make("123456")]);
+        Admin::create(["name"=>"Razon","first_surname"=>"Miah","password"=>Hash::make("12345678"),"email"=>"miahrazon@gmail.com","can_create"=>1]);
+        Admin::create(["name"=>"Clara","first_surname"=>"Ameller","password"=>Hash::make("12345678"),"email"=>"cameller@gmail.com","can_create"=>1]);
+        Admin::create(['name'=>"Adria",'first_surname'=>"Vinas",'email'=>"avinas@gmail.com",'password'=>Hash::make("NtRnStR1617"),"can_create"=>1]);
+        Admin::create(['name'=>"Admin",'first_surname'=>"Test",'email'=>"testadmin@gmail.com",'password'=>Hash::make("12345678")]);
 
         Plan::create(['name' => "charming", "price" => 9.95]);
         Plan::create(['name' => "pro", "price" => 18.95]);
         Plan::create(['name' => "premium", "price" => 29.95]);
 
+        Transporter::create(['name' => "Seur", "CIF" => "A96321456","phone_number"=>934567891]);
+        Transporter::create(['name' => "Envialia", "CIF" => "Z54258645","phone_number"=>932354655]);
+        Transporter::create(['name' => "Transportes Ramos y Cajal", "CIF" => "C56432186","phone_number"=>936857846]);
         //brands
         foreach (range(1, 250) as $index) {
             $brands = Brand::create([
@@ -64,7 +68,7 @@ class TableSeeders extends Seeder
         }
 
         //users with plan
-        foreach (range(1, 300) as $index) {
+        foreach (range(1, 350) as $index) {
             $user = User::create([
                 'name' => $faker->name,
                 'first_surname' => $faker->lastName,
