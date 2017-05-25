21e889b1684f7f35bec65f5c5c0f623e2e6e918e
Razon <x6698720q@gmail.com>
2017-05-25 18:33:14 +0000

    new migration changing deliveries columns

--- app/Http/Controllers/Box/BoxController.php
+++ app/Http/Controllers/Box/BoxController.php
@@ -9,36 +9,77 @@ use App\Http\Controllers\Controller;
 
 class BoxController extends Controller
 {
+    /**
+     * Get all products with user allergies or user unliked ingredient product
+     * @param $user
+     * @return mixed
+     */
+    private function products($user){
+        //products with allergies thats user has
+        $productsWithUserAllergies=DB::table('ingredient_product')
+            ->join('allergy_ingredient', 'ingredient_product.ingredient_id', '=', 'allergy_ingredient.ingredient_id')
+            ->join('allergy_user', 'allergy_ingredient.allergy_id', '=', 'allergy_user.allergy_id')
+            ->select('ingredient_product.product_id')
+            ->where('allergy_user.user_id','=',$user->id)
+            ->distinct()
+            ->get();
+        $query=DB::table('products');
+        foreach ($productsWithUserAllergies as $item){
+            $query->where('products.id','<>',$item->product_id);
+        }
 
-    public static function makeBox(){
+        $ingProducts = App\Product::whereDoesntHave('ingredients', function ($query) use ($user) {
+            foreach ($user->ingredients as $ing){
+                $query->where('ingredients.id', '!=', $ing->id);
+            }
+        })->get();
+        foreach ($ingProducts as $item){
+            $query->where('products.id','<>',$item->id);
+        }
+
+        return $query->get();
+    }
+
+
+    public  function makeBox(){
 
        $usersWithPlan=App\User::has("plan")->get();
         foreach ($usersWithPlan as $user) {
 
-            switch ($user->plan()){
+            switch ($user->plan->id){
 
+                case 1:
+
+
+                    break;
             }
        }
+     $user=App\User::find(1701);
+     $products=$this->products($user);
 
-       //products with allergies thats user has
-       $productsWithUserAllergies=DB::table('ingredient_product')
-           ->join('allergy_ingredient', 'ingredient_product.ingredient_id', '=', 'allergy_ingredient.ingredient_id')
-           ->join('allergy_user', 'allergy_ingredient.allergy_id', '=', 'allergy_user.allergy_id')
-           ->join('products', 'ingredient_product.product_id', '=', 'products.id')
-           ->select('products.*')
-           ->where('allergy_user.user_id','=',1701)
-           ->distinct();
+        $box=new App\Box;
+        $box->save();
+        foreach ($products as $product){
+            $box->products()->attach([$product->id]);
+        }
+        $box->save();
+
+        $delivery=new App\Delivery;
+        $delivery->save();
+        $transporter= App\Transporter::find(rand(1,3));
+        $delivery->user()->associate($user);
+        $delivery->transporter()->associate($transporter);
+        $delivery->box()->associate($box);
+        $delivery->save();
 
 
+       //get all users with plan   ------------ok
 
+        //por cada user..------------------ok
+            //get user ingredient d------------------ok
+            //get user allergies------------------ok
 
-       //get all users with plan
-
-        //por cada user..
-            //get user ingredient d
-            //get user allergies
-
-            //get products sin dichos ingredientes y allergies
+            //get products sin dichos ingredientes y allergies------------------ok
 
             //switch plan_id user
 
@@ -47,6 +88,8 @@ class BoxController extends Controller
 
 
         //
-        return $productsWithUserAllergies;
+        return $products;
     }
+
+
 }
