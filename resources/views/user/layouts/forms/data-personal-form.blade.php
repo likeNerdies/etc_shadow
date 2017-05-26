2632a92eb7b1a51643a1a78268949f4e301240fe
adriavinias <adria.vinas.pascual@gmail.com>
2017-05-26 16:22:26 +0200

    Falta retocar el formulario, para que se vea bien en formato movil

--- resources/views/user/layouts/forms/data-personal-form.blade.php
+++ resources/views/user/layouts/forms/data-personal-form.blade.php
@@ -3,35 +3,30 @@
         {{ csrf_field() }}
         {{method_field('PUT')}}
 
-        <div class="form-group{{ $errors->has('dni') ? ' has-error' : '' }}">
-            <label for="name" class="col-md-4 control-label">Dni</label>
-
-            <div class="col-md-6">
+        <div class="d-flex flex-md-row display-767-column mb-4">
+            <div class="group-input">
+                <label for="name" class="col-form-label">Dni</label>
                 @if (count($errors) > 0)
-                    <input id="dni" type="text" class="form-control" name="dni" value="{{ old('dni') }}" required autofocus
+                    <input id="dni" type="text" class="form-control col-md-11 col-12" name="dni" value="{{ old('dni') }}" required autofocus
                            placeholder="document id">
                 @else
-                    <input id="dni" type="text" class="form-control" name="dni" value="{{ Auth::user()->dni }}" required
-                           autofocus placeholder="document id">
+                    <input id="dni" type="text" class="form-control col-md-11 col-12" name="dni" value="{{ Auth::user()->dni }}" required
+                           autofocus placeholder="Document id">
                 @endif
 
                 @if ($errors->has('dni'))
                     <span class="help-block">
-                        <strong>{{ $errors->first('dni') }}</strong>
-                    </span>
+                    <strong>{{ $errors->first('dni') }}</strong>
+                </span>
                 @endif
             </div>
-        </div>
-
-        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-            <label for="name" class="col-md-4 control-label">Name</label>
-
-            <div class="col-md-6">
+            <div class="group-input">
+                <label for="name" class="col-form-label">Name</label>
                 @if (count($errors) > 0)
-                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required
+                    <input id="name" type="text" class="form-control col-md-11 col-12" name="name" value="{{ old('name') }}" required
                            autofocus>
                 @else
-                    <input id="name" type="text" class="form-control" name="name" value="{{ Auth::user()->name }}" required
+                    <input id="name" type="text" class="form-control col-md-11 col-12" name="name" value="{{ Auth::user()->name }}" required
                            autofocus>
                 @endif
 
@@ -42,81 +37,84 @@
                 @endif
             </div>
         </div>
-
-        <div class="form-group{{ $errors->has('first_surname') ? ' has-error' : '' }}">
-            <label for="name" class="col-md-4 control-label">First surname</label>
-            <div class="col-md-6">
-                @if(count($errors)>0)
-                    <input id="first_surname" type="text" class="form-control" name="first_surname"
-                           value="{{ old('first_surname') }}" required autofocus>
+        <!--  surname second_surname-->
+        <div class="d-flex flex-md-row display-767-column mb-4">
+            <div class="group-input">
+                <label for="name" class="col-form-label">First surname</label>
+                @if (count($errors) > 0)
+                    <input id="first_surname" type="text" class="form-control col-md-11 col-12" name="first_surname" value="{{ old('first_surname') }}" required autofocus
+                           placeholder="First surname">
                 @else
-                    <input id="first_surname" type="text" class="form-control" name="first_surname"
-                           value="{{ Auth::user()->first_surname }}" required autofocus>
+                    <input id="first_surname" type="text" class="form-control col-md-11 col-12" name="first_surname" value="{{ Auth::user()->first_surname }}" required
+                           autofocus placeholder="First surname">
                 @endif
 
                 @if ($errors->has('first_surname'))
                     <span class="help-block">
-                        <strong>{{ $errors->first('first_surname') }}</strong>
-                    </span>
+                    <strong>{{ $errors->first('first_surname') }}</strong>
+                </span>
                 @endif
             </div>
-        </div>
 
-        <div class="form-group{{ $errors->has('second_surname') ? ' has-error' : '' }}">
-            <label for="name" class="col-md-4 control-label">Second surname</label>
-            <div class="col-md-6">
-                @if(count($errors)>0)
-                    <input id="second_surname" type="text" class="form-control" name="second_surname"
-                           value="{{ old('second_surname') }}" autofocus>
+            <div class="group-input">
+                <label for="name" class="col-form-label">Second surname</label>
+                @if (count($errors) > 0)
+                    <input id="second_surname" type="text" class="form-control col-md-11 col-12" name="second_surname" value="{{ old('second_surname') }}" required autofocus
+                           placeholder="Second surname">
                 @else
-                    <input id="second_surname" type="text" class="form-control" name="second_surname"
-                           value="{{ Auth::user()->second_surname }}" autofocus placeholder="second surname">
+                    <input id="second_surname" type="text" class="form-control col-md-11 col-12" name="second_surname" value="{{ Auth::user()->second_surname }}" required
+                           autofocus placeholder="Second surname">
                 @endif
 
                 @if ($errors->has('second_surname'))
                     <span class="help-block">
-                        <strong>{{ $errors->first('second_surname') }}</strong>
-                    </span>
+                    <strong>{{ $errors->first('second_surname') }}</strong>
+                </span>
                 @endif
             </div>
+
         </div>
 
-        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+        <!-- email -->
 
-            <div class="col-md-6">
-                @if(count($errors)>0)
-                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+        <div class="d-flex flex-md-row display-767-column mb-4">
+            <div class="group-input">
+                <label for="email" class="col-form-label">Email</label>
+                @if (count($errors) > 0)
+                    <input id="email" type="text" class="form-control col-md-11 col-12" name="email" value="{{ old('email') }}" required autofocus
+                           placeholder="Email">
                 @else
-                    <input id="email" type="email" class="form-control" name="email" value="{{ Auth::user()->email }}"
-                           required>
+                    <input id="email" type="text" class="form-control col-md-11 col-12" name="email" value="{{ Auth::user()->email }}" required
+                           autofocus placeholder="Email">
                 @endif
+
                 @if ($errors->has('email'))
                     <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
                 @endif
             </div>
-        </div>
 
-        <div class="form-group{{ $errors->has('phone_number') ? ' has-error' : '' }}">
-            <label for="phone_number" class="col-md-4 control-label">Phone number</label>
-            <div class="col-md-6">
-                @if(count($errors)>0)
-                    <input id="phone_number" type="text" class="form-control" name="phone_number"
-                           value="{{ old('phone_number') }}" autofocus placeholder="phone number">
+            <div class="group-input">
+                <label for="phone_number" class="col-form-label">Phone number</label>
+                @if (count($errors) > 0)
+                    <input id="phone_number" type="text" class="form-control col-md-11 col-12" name="phone_number" value="{{ old('phone_number') }}" required autofocus
+                           placeholder="Phone number">
                 @else
-                    <input id="phone_number" type="text" class="form-control" name="phone_number"
-                           value="{{ Auth::user()->phone_number }}" autofocus placeholder="phone number">
+                    <input id="phone_number" type="text" class="form-control col-md-11 col-12" name="phone_number" value="{{ Auth::user()->phone_number }}" required
+                           autofocus placeholder="Phone number">
                 @endif
 
                 @if ($errors->has('phone_number'))
                     <span class="help-block">
-                        <strong>{{ $errors->first('phone_number') }}</strong>
-                    </span>
+                    <strong>{{ $errors->first('phone_number') }}</strong>
+                </span>
                 @endif
             </div>
+
         </div>
+
+        <!-- email -->
 
         <div class="form-group">
             <div class="col-md-6 col-md-offset-6">
