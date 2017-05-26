4f601800e42e4f6c7bc20107296b350a4c4d805a
Razon <x6698720q@gmail.com>
2017-05-26 23:09:23 +0000

    added functionality like / unlike ingredient uuser

--- public/js/user/ingredients/ingredients.js
+++ public/js/user/ingredients/ingredients.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
 
     $(".card").click(function (e) {
-
+        var unlike=true;
+        var url='/user/panel/ingredients/unlike';
+        if ( $( this ).hasClass( "unlike" ) ) {
+            unlike=false;
+            url='/user/panel/ingredients/like';
+        }
+        var div=$(this );
         $(this).toggleClass("ingredient-selected");
-
         var ingredient_id = $(this).attr('id');
         var _token = $('input[name="_token"]').val();
         var datas = {"ingredient_id": ingredient_id, _token: _token};
@@ -11,16 +16,27 @@ $(document).ready(function () {
 
         $.ajax({
             type: "POST",
-            url: '/user/panel/ingredients/unlike',
+            url:url ,
             data: datas,
             dataType: 'json',
             success: function (data) {
-                $(this).css(" border", "1px solid #bf2c1b");
+               if(unlike){
+                   if (div.hasClass( "like" )){
+                       div.removeClass('like');
+                       console.log('unlike')
+                   }
+                   div.addClass('unlike')
+
+               }else{
+                   if (div.hasClass( "unlike" )){
+                       div.removeClass('unlike');
+                       console.log('like')
+                   }
+                   div.addClass('like')
+               }
             },
-            error: function (xhr, ajaxOptions, thrownError) {
-                console.log(xhr.status);
-                console.log(xhr.responseText);
-                console.log(thrownError);
+            error: function (data) {
+                //todo
             }
         });
     });
--- resources/views/user/panel/ingredient/index.blade.php
+++ resources/views/user/panel/ingredient/index.blade.php
@@ -10,18 +10,14 @@
                 foreach ($ingredients as $ingredient){
                       $userUnlikeIngredients=Auth::user()->ingredients;
                       $exists=false;
-                      $style="border:1px solid #42aa55";
                       $class="like";
                       for ($i=0;$i<count($userUnlikeIngredients)&& !$exists;$i++){
                               if($ingredient->id == $userUnlikeIngredients[$i]->id){
                                   $exists=true;
+                                   $class="unlike";
                               }
                       }
-                    if($exists){
-                         $style="border:1px solid #bf2c1b";
-                         $class="unlike";
-                    }
-                      echo  '<div id="'.$ingredient->id.'" class="card p-2 col-md-3 mx-1 '.$class.'" style="'.$style.'">';
+                      echo  '<div id="'.$ingredient->id.'" class="card p-2 col-md-3 mx-1 '.$class.'">';
                       echo  ' <img src="/user/panel/ingredients/'.$ingredient->id .'/image" class="rounded card-img-top ingredient-img img-fluid pt-3 pb-3" alt="'. $ingredient->name.'">';
                       echo ' <div class="card-footer">';
                       echo '<small>', $ingredient->name,'</small>';
@@ -29,14 +25,6 @@
 
                 }
             @endphp
-
-            {{--      <div id=" {{ $ingredient->id }} " class="card p-2 col-md-3 mx-1">
-                    <img src="/user/panel/ingredients/{{ $ingredient->id }}/image" class="rounded card-img-top ingredient-img img-fluid pt-3 pb-3" alt=" {{ $ingredient->name }} ">
-                    <div class="card-footer">
-                        <small> {{ $ingredient->name }} </small>
-                    </div>
-                  </div>
-          --}}
         </div>
 
     </div>
