fca6402ef6c5231e2e3cc03f69257edf77ffeee7
ClaraAmeller <claraameller7@gmail.com>
2017-05-29 08:11:48 +0000

    Navbar /user/panel/my-data style

--- resources/views/user/layouts/forms/data-personal-form.blade.php
+++ resources/views/user/layouts/forms/data-personal-form.blade.php
@@ -7,10 +7,10 @@
             <div class="group-input">
                 <label for="name" class="col-form-label">Dni</label>
                 @if (count($errors) > 0)
-                    <input id="dni" type="text" class="form-control col-md-11 col-12" name="dni" value="{{ old('dni') }}" required autofocus
+                    <input id="dni" type="text" class="form-control col-12" name="dni" value="{{ old('dni') }}" required autofocus
                            placeholder="document id">
                 @else
-                    <input id="dni" type="text" class="form-control col-md-11 col-12" name="dni" value="{{ Auth::user()->dni }}" required
+                    <input id="dni" type="text" class="form-control col-12" name="dni" value="{{ Auth::user()->dni }}" required
                            autofocus placeholder="Document id">
                 @endif
 
@@ -20,13 +20,13 @@
                 </span>
                 @endif
             </div>
-            <div class="group-input">
+            <div class="group-input ml-md-4 ml-sm-0">
                 <label for="name" class="col-form-label">Name</label>
                 @if (count($errors) > 0)
-                    <input id="name" type="text" class="form-control col-md-11 col-12" name="name" value="{{ old('name') }}" required
+                    <input id="name" type="text" class="form-control col-12" name="name" value="{{ old('name') }}" required
                            autofocus>
                 @else
-                    <input id="name" type="text" class="form-control col-md-11 col-12" name="name" value="{{ Auth::user()->name }}" required
+                    <input id="name" type="text" class="form-control col-12" name="name" value="{{ Auth::user()->name }}" required
                            autofocus>
                 @endif
 
@@ -42,10 +42,10 @@
             <div class="group-input">
                 <label for="name" class="col-form-label">First surname</label>
                 @if (count($errors) > 0)
-                    <input id="first_surname" type="text" class="form-control col-md-11 col-12" name="first_surname" value="{{ old('first_surname') }}" required autofocus
+                    <input id="first_surname" type="text" class="form-control col-12" name="first_surname" value="{{ old('first_surname') }}" required autofocus
                            placeholder="First surname">
                 @else
-                    <input id="first_surname" type="text" class="form-control col-md-11 col-12" name="first_surname" value="{{ Auth::user()->first_surname }}" required
+                    <input id="first_surname" type="text" class="form-control col-12" name="first_surname" value="{{ Auth::user()->first_surname }}" required
                            autofocus placeholder="First surname">
                 @endif
 
@@ -56,13 +56,13 @@
                 @endif
             </div>
 
-            <div class="group-input">
+            <div class="group-input ml-md-4 ml-sm-0">
                 <label for="name" class="col-form-label">Second surname</label>
                 @if (count($errors) > 0)
-                    <input id="second_surname" type="text" class="form-control col-md-11 col-12" name="second_surname" value="{{ old('second_surname') }}" required autofocus
+                    <input id="second_surname" type="text" class="form-control col-12" name="second_surname" value="{{ old('second_surname') }}" required autofocus
                            placeholder="Second surname">
                 @else
-                    <input id="second_surname" type="text" class="form-control col-md-11 col-12" name="second_surname" value="{{ Auth::user()->second_surname }}" required
+                    <input id="second_surname" type="text" class="form-control col-12" name="second_surname" value="{{ Auth::user()->second_surname }}" required
                            autofocus placeholder="Second surname">
                 @endif
 
@@ -81,10 +81,10 @@
             <div class="group-input">
                 <label for="email" class="col-form-label">Email</label>
                 @if (count($errors) > 0)
-                    <input id="email" type="text" class="form-control col-md-11 col-12" name="email" value="{{ old('email') }}" required autofocus
+                    <input id="email" type="text" class="form-control col-12" name="email" value="{{ old('email') }}" required autofocus
                            placeholder="Email">
                 @else
-                    <input id="email" type="text" class="form-control col-md-11 col-12" name="email" value="{{ Auth::user()->email }}" required
+                    <input id="email" type="text" class="form-control col-12" name="email" value="{{ Auth::user()->email }}" required
                            autofocus placeholder="Email">
                 @endif
 
@@ -95,13 +95,13 @@
                 @endif
             </div>
 
-            <div class="group-input">
+            <div class="group-input ml-md-4 ml-sm-0">
                 <label for="phone_number" class="col-form-label">Phone number</label>
                 @if (count($errors) > 0)
-                    <input id="phone_number" type="text" class="form-control col-md-11 col-12" name="phone_number" value="{{ old('phone_number') }}" required autofocus
+                    <input id="phone_number" type="text" class="form-control col-12" name="phone_number" value="{{ old('phone_number') }}" required autofocus
                            placeholder="Phone number">
                 @else
-                    <input id="phone_number" type="text" class="form-control col-md-11 col-12" name="phone_number" value="{{ Auth::user()->phone_number }}" required
+                    <input id="phone_number" type="text" class="form-control col-12" name="phone_number" value="{{ Auth::user()->phone_number }}" required
                            autofocus placeholder="Phone number">
                 @endif
 
@@ -117,8 +117,8 @@
         <!-- email -->
 
         <div class="d-flex flex-md-row display-767-column mb-4">
-            <div class="group-input">
-                <button type="submit" class="btn btn-primary">
+            <div class="group-input mt-3">
+                <button type="submit" class="btn btn-primary float-right">
                     Save
                 </button>
             </div>
