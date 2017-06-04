1a0e99f403a38ad5fbace93202d304c4de5a611d
Razon <x6698720q@gmail.com>
2017-06-04 21:24:27 +0000

    lang for product

--- resources/lang/en/product/product.php
+++ resources/lang/en/product/product.php
@@ -4,5 +4,9 @@ return[
     "description"=>"Description",
     "ingredients"=>"Ingredients",
     "next"=>"Next",
-    "previous"=>"Previous"
+    "previous"=>"Previous",
+    "love_our_products"=>"Fall in <strong>love</strong> with our products",
+    "vegan"=>"Vegan",
+    "organic"=>"Organic",
+    "vegetarian"=>"Vegetarian",
 ];
