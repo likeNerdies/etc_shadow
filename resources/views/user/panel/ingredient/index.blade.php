1e83214002d9f760a7a7ac145eeb1da49a5123d3
ClaraAmeller <claraameller7@gmail.com>
2017-06-05 07:24:02 +0000

    Allergies/Ingredients Search input

--- resources/views/user/panel/ingredient/index.blade.php
+++ resources/views/user/panel/ingredient/index.blade.php
@@ -1,20 +1,26 @@
 @extends('user.layouts.panel')
 @section('title','My ingredients')
 @section('right-panel')
-    <div class="container ml-3rem">
-        <div class="row">
+    <div class="mar-auto">
+        <!--<div class="row mt-3">-->
             <div class="col-11 offset-sm-1 mx-auto text-center">
                 <div class="alert alert-warning mb-5" role="alert">
-                    <h5 class="allergy-ingredient-warning text-warning"><strong>@lang('user/ingredient/ingredient.hey')</strong> @lang('user/ingredient/ingredient.make_sure')</h5>
+                    <h5 class="allergy-ingredient-warning text-warning">
+                      <strong>@lang('user/ingredient/ingredient.hey')</strong> @lang('user/ingredient/ingredient.make_sure')
+                    </h5>
                 </div>
             </div>
+        <!--</div>-->
+
+      <!--<div class="row">-->
+        <div class="col-md-6 ml-2_7rem">
+          <input  type="text" id="search" class="form-control" name="ingredient" placeholder="Search">
         </div>
+      <!--</div>-->
 
-
-        <div class="row">
-            <div class="col-md-11 col-12 mx-auto">
-                <div><input  type="text" id="search" name="ingredient" placeholder="search"></div>
-              <div class="error mt-3 text-center"></div>
+        <!--<div class="row">-->
+            <div class="col-md-11 mx-auto">
+                <div class="error mt-3 text-center"></div>
                 <div id="ingredients_user" class="card-columns text-center">
                     {{ csrf_field() }}
                     @php
@@ -28,7 +34,7 @@
                                            $class = "unlike";
                                       }
                               }
-                              echo '<div class="card-wrapper m-1">';
+                              echo '<div class="card-wrapper">';
                               echo  '<div id="'.$ingredient->id.'" class="card mx-auto '.$class.'">';
                               echo  ' <img src="/user/panel/ingredients/'.$ingredient->id .'/image" class="rounded card-img-top ingredient-img img-responsive mx-auto py-3" alt="'. _t($ingredient->name,[],Session::get('locale')).'">';
                               echo ' <div class="card-footer">';
@@ -39,7 +45,7 @@
                     @endphp
                 </div>
             </div>
-        </div>
+        <!--</div>-->
     </div>
 @endsection
 
