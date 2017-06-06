e3f11abc79475b79e8de8bb0aff1bbe93f592543
ClaraAmeller <claraameller7@gmail.com>
2017-06-06 21:08:27 +0000

    Softer expire red + Help translated

--- resources/lang/en/help.php
+++ resources/lang/en/help.php
@@ -2,21 +2,22 @@
 
 return [
   "needHelp" => "Do you need help?",
-  "toSubscribe" => "How to subscribe to a <a href='/help'>plan</a>",
-  "subscribeOptions" => "You have two options:",
-  "subscribe1" => "1. Click to the 'Plans' section on your left",
-  "subscribe2" => "2. Or click on the 'Plans' navbar item",
+  "toSubscribe" => "How to subscribe to a <a href='/plans'>plan</a>",
+  "subscribeOptions" => "It's easier than you thought",
+  "subscribe1" => "You can see all the products we have for you on this <a href='/plans'>link</a>.",
+  "subscribe2" => "Choose the plan you like and hit the <strong>Subscribe</strong> button.",
+  "subscribe3" => "You'll be redirected to a paying page where you'll fill a form with your bank details.",
 
-  "seeProducts" => "How to see our products",
-  "productsOption1" => "You can click to the 'Products' navbar item!",
-  "productsOption2" => "Then click the product you would like to get more information of, like the description of it,the ingredients it has, etc.",
+  "seeProducts" => "How to see our <a href='/products'>products</a>",
+  "productsOption1" => "You can click the 'Products' menú on the navbar! Or just by clicking this <a href='/products'>link</a>.",
+  "productsOption2" => "You can see all te products we have for you, search by brand, diets and categories.",
   "productsOption3" => "",
 
-  "ingredientsAllergies" => "How to set the ingredients you don't like and your allergies",
-  "personalizeOption1" => "Click on the 'Ingredients and Allergies' section on your left!",
-  "personalizeOption2" => "You will be driven to a form with images of ingredients. Just click on the ones you don't like and we'll make sure you don't receive anything with that!",
-  "personalizeOption3" => "To let us know your allergies, just select the ones you have on that wonderful table you'll see, or just search it on our input.",
-];
+  "ingredientsAllergies" => "How to set the ingredients you dislike and your allergies",
+  "personalizeOption1" => "Click the 'Ingredients and allergies' section on your <a href='/user/panel'>user panel</a>.",
+  "personalizeOption2" => "On the <a href='/user/panel/ingredients'>ingredients</a> page simply click the ingredientes you dislike, and we'll make sure you don't receive products with those ingredients!",
+  "personalizeOption3" => "To let us know your <a href='/user/panel/allergies'>allergies</a>, click the right column on the allergies table, and it'll appear a text: <strong>Yes</strong>, so we know you have that allergy. You can deselect it clicking it again.",
 
+];
 
 ?>
--- resources/lang/es/help.php
+++ resources/lang/es/help.php
@@ -2,21 +2,21 @@
 
 return [
   "needHelp" => "¿Necesitas ayuda?",
-  "toSubscribe" => "Cómo subscribirse a un <a href='/help'>plan</a>",
+  "toSubscribe" => "Cómo subscribirse a un <a href='/plans'>plan</a>",
   "subscribeOptions" => "Es más sencillo de lo que te pensabas ",
-  "subscribe1" => "En este <a href='/plans'>enlace</a> podrás ver todos los planes que tenemos para ti",
-  "subscribe2" => "Eliges el plan a tu gusto y pulsas en el botón de <strong>subscribirse</strong>",
-  "subscribe3" => "Se te redirigirá a una página de pago donde tendrás que rellenar un formulario con tus datos bancarios, y pagar",
+  "subscribe1" => "En este <a href='/plans'>enlace</a> podrás ver todos los planes que tenemos para ti.",
+  "subscribe2" => "Eliges el plan a tu gusto y pulsas en el botón de <strong>Subscribirse</strong>.",
+  "subscribe3" => "Se te redirigirá a una página de pago donde tendrás que rellenar un formulario con tus datos bancarios.",
 
   "seeProducts" => "Cómo ver nuestros  <a href='/products'>productos</a>",
-  "productsOption1" => "¡Puedes hacer click en el menú 'Productos' de la barra de navegación! o a través de este <a href='/products'>enlace</a> puedes acceder a productos",
-  "productsOption2" => "Puedes ver todas nuestros productos que tenemos para ti, haces busquedas por marcas, dietas o categorías",
+  "productsOption1" => "¡Puedes hacer click en el menú 'Productos' de la barra de navegación! O a través de este <a href='/products'>enlace</a>.",
+  "productsOption2" => "Puedes ver todas nuestros productos que tenemos para ti, hacer búsquedas por marcas, dietas o categorías.",
   "productsOption3" => "",
 
   "ingredientsAllergies" => "Cómo marcar los ingredientes que no te gustan y tus alergias",
-  "personalizeOption1" => "Has click en la sección de 'Ingredientes y alergias' en el <a href='/user/panel'>panel de usuario</a>",
-  "personalizeOption2" => "En la página de <a href='/user/panel/ingredients'>ingredientes</a> simplemente haz click en esos que no te gustan y nosotros nos aseguraremos de no enviarte productos con esos ingredientes!",
-  "personalizeOption3" => "Para hacernos saber tus <a href='/user/panel/allergies'>alergias</a>, en la tabla de las alergias al hacer click a la derecha de cada alergia, aparecerá un texto: <strong>Yes</strong>, con eso ya sabremos que lo tienes. Puedes desmarcarlo haciendo otro click encima",
+  "personalizeOption1" => "Haz click en la sección de 'Ingredientes y alergias' en el <a href='/user/panel'>panel de usuario</a>.",
+  "personalizeOption2" => "En la página de <a href='/user/panel/ingredients'>ingredientes</a> simplemente haz click en esos que no te gustan, ¡y nosotros nos aseguraremos de no enviarte productos con esos ingredientes!",
+  "personalizeOption3" => "Para hacernos saber tus <a href='/user/panel/allergies'>alergias</a>, en la tabla de las alergias al hacer click a la derecha de cada alergia, aparecerá un texto: <strong>Yes</strong>, con eso ya sabremos que la tienes. Puedes desmarcarla haciendo otro click encima.",
 
 ];
 
