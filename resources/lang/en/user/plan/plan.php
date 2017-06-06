a60e5f20b2eba7658318c0df46a07cc3d46fecb7
ClaraAmeller <claraameller7@gmail.com>
2017-06-06 13:47:48 +0000

    Plans

--- resources/lang/en/user/plan/plan.php
+++ resources/lang/en/user/plan/plan.php
@@ -17,18 +17,17 @@ return [
   "proTitle" => "Feeling pro and you want a box to match?",
   "proSubtitle" => "We can do that!",
   "proSlogan" => "The Pro plan is very complete and diversified.",
-  "proProductsList" => "8 products: One drink (no dairy milk or juicies), a cookies box, a cereal box and a bar pack.",
+  "proProductsList" => "8 products",
 
   "premiumTitle" => "Feeling extra? Just like this box.",
   "premiumSubtitle" => "We want you to be healthy, like, extra healthy.",
   "premiumSlogan" => "The Premium plan will change you.",
-  "premiumProductsList" => "12 products: One drink (no dairy milk or juicies), a cookies box, a cereal box and a bar pack.",
+  "premiumProductsList" => "12 products",
 
   "charmingTitle" => "Want to start from the very scratch?",
   "charmingSubtitle" => "We have the perfect plan for you!",
   "charmingSlogan" => "The Charming plan is small but plenty of delightfulness.",
-  "charmingProductsList" => "4 products: One drink (no dairy milk or juicies), a cookies box, a cereal box and a bar pack",
-  "notSufficient"=>"Keep an eye on the other plans we have for you",
+  "charmingProductsList" => "4 products",
 ];
 
 ?>
